6b970dbb22080f1f2cd733b6304913ba18bdb978
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2021-11-25 19:33:36 -0300

    Update CadLivro.php

--- adm/CadLivro.php
+++ adm/CadLivro.php
@@ -1,24 +1,3 @@
 <?php
-session_start();
-include_once ("../servidor.php");
-$tituto   = $_POST["titulo"]; 
-$desc     = $_POST["desc"];
-$valor    = $_POST["valor"];
-$cod_ed   = $_POST["ed"];
-$img   =$_FILES["arquivo"];
-$diretorio = "img/" . $img ["name"]; 
-$sqlInsert = "insert into tb_livro(cod_ed, titulo_liv, desc_liv, img_liv, valor_liv)"; 
-$sqlInsert .= " values($cod_ed, '$tituto', '$desc', '$diretorio' ,'$valor')"; 
-$res =  mysqli_query($link, $sqlInsert);
-if (mysqli_affected_rows($link)){
-    echo "Cadastrado Livro";
-    move_uploaded_file ( $img["tmp_name"] , $diretorio );
-    $cod_liv =  mysqli_insert_id($link);
-    $sqlCadlivro = "insert into tb_cad_livro values(". $_SESSION["login"]["id"]."," .$cod_liv.")";
-    mysqli_query($link, $sqlCadlivro);
-}
-else{
-    echo mysqli_error($link);
-}
-mysqli_close($link);
+
 ?>
