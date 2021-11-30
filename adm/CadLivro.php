c091ab003594d128e1073e166ba766baf713ebc3
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2021-11-30 14:43:23 -0300

    Update CadLivro.php

--- adm/CadLivro.php
+++ adm/CadLivro.php
@@ -11,16 +11,16 @@ $desc     = $_POST["desc"];
 $imagem   = $_FILES["arq"];
 $valor    = $_POST["valor"];
 $diretorio = "img/".$imagem["name"];
-
 $sql = "insert into tb_livro(cod_ed, titulo_liv, desc_liv, img_liv, valor_liv)"; 
 $sql.= " values(?, ?, ?, ? ,?)";
 $stm = $serv->prepare($sql);
-$stm->bindValue(1, $_POST["ed"]);
+$stm->bindValue(1, $cod_ed);
 $stm->bindValue(2, $tituto);
 $stm->bindValue(3, $desc);
 $stm->bindValue(4, $imagem["name"]);
 $stm->bindValue(5, $valor);
-if( $stm->execute()){
+$stm->execute();
+if($campo =  $stm->fetch(PDO::FETCH_ASSOC)){
     echo "Cadastrado Livro";
     $diretorio = "img/".$imagem["name"];
     move_uploaded_file ( $imagem["tmp_name"] , $diretorio ); 
