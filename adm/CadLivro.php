a3c3467587b5a9ce72cda3dd2a9f4e41a89f3fb4
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2021-12-03 16:18:35 -0300

    Update CadLivro.php

--- adm/CadLivro.php
+++ adm/CadLivro.php
@@ -1,19 +1,12 @@
 <?php
 session_start();
-// Incluir a conexao com o servidor
 include_once ("../servidor.php");
 
-//Referência de variaveis
-
- $cod_ed   = $_POST["ed"];
- $tituto   = $_POST["titulo"]; 
+$cod_ed   = $_POST["ed"];
+$tituto   = $_POST["titulo"]; 
 $desc     = $_POST["desc"];
- $imagem   = $_FILES["arq"];
+$imagem   = $_FILES["arq"];
 $valor    = $_POST["valor"];
-
-
-
-// Peguei sem fazer tratamento
 $diretorio = "img/".$imagem ["name"]; 
 
 
@@ -29,26 +22,15 @@ $stm->bindValue(5, $valor);
 
 if( $stm->execute()){
 
-   //executar   
-   echo "Cadastrado Livro";
-
-   
-      
-   $diretorio = "img/".$imagem["name"];
-
-   //mover a arquivo para o diretorio
-   move_uploaded_file ( $imagem["tmp_name"] , $diretorio );
-
- 
- // pegar o id de registrado, 
+  echo "Cadastrado Livro";
+  $diretorio = "img/".$imagem["name"];
+  move_uploaded_file ( $imagem["tmp_name"] , $diretorio );
   $id_livro =  $banco->lastInsertId();
-
-    //cadastrar na tabela cad_livros  
-   $sql = "insert into tb_cad_livro values(?,?)";
-   $stm = $banco->prepare($sql);
-   $stm->bindValue(1, $_SESSION['usuario']['id']);
-   $stm->bindValue(2, $id_livro);
-   $stm->execute();
+  $sql = "insert into tb_cad_livro values(?,?)";
+  $stm = $banco->prepare($sql);
+  $stm->bindValue(1, $_SESSION['usuario']['id']);
+  $stm->bindValue(2, $id_livro);
+  $stm->execute();
 
 }
 
