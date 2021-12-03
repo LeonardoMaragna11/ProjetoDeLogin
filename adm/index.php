fa8b7c9410545fa96584ae38430ce6500ccb3230
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2021-12-03 18:06:21 -0300

    Update index.php

--- adm/index.php
+++ adm/index.php
@@ -1,53 +1,53 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <!-- Bootstrap CSS -->
-   <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
-     <title>Document</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+    <title>Document</title>
 </head>
 
 <body>
-     <div class="container">
-         <div class="row pt-5">  
-             <div class="col-md-4 "></div> 
-             <section class="col-md-4">
-                <?php 
+    <div class="container">
+        <div class="row pt-5">
+            <div class="col-md-4 "></div>
+            <section class="col-md-4">
 
-                    if(isset($_SESSION['erro'])){
-                        echo($_SESSION['erro']);
-                        unset($_SESSION['erro']);
-                    }
-
-                
-                ?>
                 <form action="procLogin.php" method="post">
                     <fieldset>
-                        <legend>Identifique</legend>                        
+                        <legend>Identifique</legend>
+                        <?php 
+                            if(isset($_SESSION['erro'])){
+                                echo($_SESSION['erro']);
+                                unset($_SESSION['erro']);
+                            }
+                        ?>
                         <div>
                             <label for="login"> Login :</label>
-                            <input type="text" class="form-control" name="login"  id="login">
-                        </div>                        
+                            <input type="text" class="form-control" name="login" id="login">
+                        </div>
                         <div>
                             <label for="senha"> Senha :</label>
-                            <input type="password" class="form-control" name="senha"  id="senha">
+                            <input type="password" class="form-control" name="senha" id="senha">
                         </div>
                         <br>
                         <button type="submit" class="btn btn-primary">Acesso</button>
-                        
+
                     </fieldset>
                 </form>
-                <br>    
-                <a href="../index.php"><button class="btn btn-danger" >Voltar</button></a>
+                <br>
+                <a href="../index.php"><button class="btn btn-danger">Voltar</button></a>
             </section>
-         <section class="col-4"></section> 
-      </div>
-     </div>
+            <section class="col-4"></section>
+        </div>
+    </div>
 
 
 </body>
-<script src="../js/jquery-3.5.1.slim.min.js" ></script>
-   <script src="../js/popper.min.js" ></script>
+<script src="../js/jquery-3.5.1.slim.min.js"></script>
+<script src="../js/popper.min.js"></script>
+
 </html>
